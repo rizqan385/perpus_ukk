e789070c5e461702bdce2dca2883e20dd7507f48
rizqan385 <fa583822@gmail.com>
2026-04-16 22:35:47 +0700

    update appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->configureDefaults();
+
+    if (config('app.env') === 'production') {
+        \URL::forceScheme('https');
+    }
     }
 
     protected function configureDefaults(): void
